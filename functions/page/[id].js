6c850c2b77d13282cebab4a0b67c0bafd19f51c7
yajiezh <96071651+yajiezh@users.noreply.github.com>
2025-04-15 00:51:57 +0800

    Update [id].js

--- functions/page/[id].js
+++ functions/page/[id].js
@@ -1,14 +1,11 @@
 export async function onRequestGet({ params, env }) {
-    const html = await env.HTML_STORE.get(params.id);
+    const content = await env.HTML_STORE.get(params.id);
     
-    if (!html) {
-        return new Response('页面不存在', { 
-            status: 404,
-            headers: { 'Content-Type': 'text/html' }
-        });
-    }
-
-    return new Response(html, {
-        headers: { 'Content-Type': 'text/html' }
+    return new Response(content || 'Not Found', {
+        status: content ? 200 : 404,
+        headers: { 
+            'Content-Type': 'text/html',
+            'X-Frame-Options': 'DENY'
+        }
     });
 }
