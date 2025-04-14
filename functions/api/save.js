62afc7a0469b9f6e9e1c41f77807e19f98b89226
yajiezh <96071651+yajiezh@users.noreply.github.com>
2025-04-15 00:52:31 +0800

    Update save.js

--- functions/api/save.js
+++ functions/api/save.js
@@ -1,16 +1,18 @@
 export default {
-    async post(request, env) {
+    async post({ request, env }) {
         const html = await request.text();
         const id = crypto.randomUUID();
         
         await env.HTML_STORE.put(id, html, {
-            metadata: {
-                createdAt: Date.now()
-            }
+            metadata: { created: Date.now() }
         });
-
+        
         return new Response(JSON.stringify({ id }), {
-            headers: { 'Content-Type': 'application/json' }
+            headers: { 
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Cache-Control': 'no-store'
+            }
         });
     }
 }
